f4498917adae3fce58864c4385add3971790d6c9
David Tetreau <david_tetreau@yahoo.com>
2021-04-04 19:25:53 -0500

    automatic color change

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+
 const colors = ["AntiqueWhite", "AliceBlue", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "BlanchedAlmond", "BlueViolet", "Brown", "Burlywood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Green","GreenYellow","HoneyDew", "HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid", "MediumPurple","MediumSeaGreen","MediumSlateBlue", "MediumSpringGreen","MediumTurqiose","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin", "NavajoWhite","Navy","Oldlace", "Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple", "RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","Seashell","Sienna", "Silver","SkyBlue","SlateBlue","SlateGrey","Snow","SpringGreen","SteelBlue","Tan", "Teal","Thistle", "Tomato","Turquoise", "Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen","" ];
 const color = document.querySelector(".color");
 
-function letsGetItNow() {  
-    const randomNumber = getRandomNumber();// get random number between 0 - 3 
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
-    letsGetItNow();
+setInterval(() => {
+        const randomNumber = getRandomNumber();
+        document.body.style.backgroundColor = colors[randomNumber];
+        color.textContent = colors[randomNumber];
+}, 3000);
+
+const getRandomNumber = () => {
+   return Math.floor(Math.random() * colors.length);
 }
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * colors.length);
-}
