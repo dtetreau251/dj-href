af5cd1a71b89fddfa70fffca3232a6189580f1a8
David Tetreau <david_tetreau@yahoo.com>
2021-04-04 19:34:50 -0500

    changed color change to container

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const color = document.querySelector(".color");
 
 setInterval(() => {
         const randomNumber = getRandomNumber();
-        document.body.style.backgroundColor = colors[randomNumber];
+        document.container.style.backgroundColor = colors[randomNumber];
         color.textContent = colors[randomNumber];
 }, 3000);
 
